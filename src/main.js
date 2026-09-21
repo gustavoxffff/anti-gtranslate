6387daddac37629997907b87511c7fe6de439195
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2026-09-21 10:54:22 -0300

    feat: added more verification and scroll to bottom up again

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 const clicked = new Set();
 
+const languagesToTranslate = ["English"];
+
 function clickTranslateButtons() {
   document.querySelectorAll('span[role="button"]').forEach((s) => {
     if (s.hasAttribute("aria-label")) return;
 
     const action = s.getAttribute("jsaction");
+
     if (!action || clicked.has(action)) return;
 
+    const hasLanguage = languagesToTranslate.some(lang => s.innerHTML.includes(lang));
+    if (!hasLanguage) return;
+
     clicked.add(action);
+
     s.click();
+    setTimeout(() => {
+      scroll({ top: 0, behavior: "instant" });
+    }, 50);
   });
 }
 
